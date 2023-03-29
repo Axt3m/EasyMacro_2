a8ed1e893aeea88159a8171e1ae4dda3264729fd
Axt3m <angela.terao@gmail.com>
2023-03-29 11:37:20 +0200

    CalculatorMacro modifications

--- EasyMacro/Controllers/ResultsViewController.swift
+++ EasyMacro/Controllers/ResultsViewController.swift
@@ -10,5 +10,25 @@ import UIKit
 class ResultsViewController: UIViewController {
     
     
+    var userChoices = UserChoices()
+    var gender: String?
+    var weight: Int?
+    var activity: String?
+    var sports: String?
+    var goals: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(Test.unwrapOptionalString(gender))
+        print(Test.unwrapOptionalInt(weight))
+        print(Test.unwrapOptionalString(activity))
+        print(Test.unwrapOptionalString(sports))
+        print(Test.unwrapOptionalString(goals))
+        
+        var calculatorMacro = CalculatorMacro(userChoices: UserChoices(gender: gender, weight: weight, activity: activity, sport: sports, goals: goals))
+        
+        calculatorMacro.userData()
+    }
     
 }
